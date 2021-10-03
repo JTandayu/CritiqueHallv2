9e795010233d6ffe6eb7a2478be47f1a3910ff53
Jose Tandayu <jose.tandayu@gmail.com>
2021-10-03 17:21:24 +0800

    Added Nav and Layout page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+import Layout from './layout/layout'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+          <Component {...pageProps} />
+  ) 
 }
-
-export default MyApp
---
+++ pages/layout/layout.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { css, cx } from '@emotion/react'
+import { motion } from "framer-motion"
+import Nav from "../component/nav"
+import styles from '../../styles/layout/Layout.module.css'
+
+export default function Layout({children}){
+    return(
+        <>
+        <Nav />
+        <main>{children}</main>
+        </>
+    )
+
+}
