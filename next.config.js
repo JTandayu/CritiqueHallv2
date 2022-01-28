9765435493d61aa47ca007277536c8cf0845e0a4
Richie Tan <richiegenetan14@gmail.com>
2022-01-28 09:40:46 +0800

    Added HTTP Security Headers: X-Frame-Options, X-XSS-Protection, and  X-Content-Type-Options.

--- next.config.js
+++ next.config.js
@@ -13,4 +13,26 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          }
+        ]
+      }
+    ]
+  }
 }
