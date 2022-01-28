0cc0bf2332a72476cacf6cb4dfe1b0dec96333e8
Richie Tan <richiegenetan14@gmail.com>
2022-01-28 09:51:55 +0800

    Added more HTTP Security Headers

--- next.config.js
+++ next.config.js
@@ -30,6 +30,18 @@ module.exports = {
           {
             key: 'X-XSS-Protection',
             value: '1; mode=block',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
           }
         ]
       }
