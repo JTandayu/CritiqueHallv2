3d9ba7055efd4b222c5e9e2219fd08d8e09cb662
Jose Tandayu <jose.tandayu@gmail.com>
2022-02-11 22:53:48 +0800

    Delete Attachment Button in Create Post Changes

--- component/post/create-post.js
+++ component/post/create-post.js
@@ -309,7 +309,7 @@ function CreatePost({data}) {
                             {fileName.map((file, i) => (
                                 <Flex ml={5} id={file} w="full">
                                     <Text fontSize='sm' key={i}>{file}</Text>
-                                    <Button onClick={()=>{deleteFile(file, i)}} mx='auto' h={5} ml={1} variant='ghost'>X</Button>
+                                    <Button onClick={()=>{deleteFile(file, i)}} mx='auto' h={5} ml={1} variant='ghost'><Image src="/cross_icon.png" w="15px" h="15px" /></Button>
                                 </Flex>
                             ))}
                         </Flex>
