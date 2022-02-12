52eed46e6c713fc865beb84ad2b9378ea1703a38
Jose Tandayu <jose.tandayu@gmail.com>
2022-02-12 14:24:24 +0800

    Changes in Notification Interval

--- pages/nav/nav.js
+++ pages/nav/nav.js
@@ -192,7 +192,7 @@ export default function Nav(data, profile_pic){
             .catch(error => {
                 // console.log(error.response.data.error);
             });
-        }, 15000)
+        }, 300000)
 
         
     }, [])
