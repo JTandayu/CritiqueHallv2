4cd2a322a34b5725d922f16e16f8fc55ac9aef1c
Jose Tandayu <jose.tandayu@gmail.com>
2022-02-14 16:07:09 +0800

    Department Dropdown Changes

--- pages/register.js
+++ pages/register.js
@@ -345,7 +345,7 @@ export default function Register({data2}) {
                 <Select borderColor={useColorModeValue('black', 'white')} borderRadius="lg" className={styles.input_select} size="sm" onChange={e => getSpecList(e.target.value)}>
                   <option value="" disabled selected>Choose</option>
                   {depList.map((department, i) => (
-                    <option disabled={department.name === "College" || department.name === "Senior High School" ? true : null} value={department.name} key={i}>{department.name}</option>
+                    <option disabled={department.name === "College" || department.name === "Senior High School" || department.name === "Faculty" ? true : null} value={department.name} key={i}>{department.name}</option>
                   ))}
                 </Select>
                 </Box>
