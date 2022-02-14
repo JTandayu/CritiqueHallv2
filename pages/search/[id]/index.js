57185c7cd41efe4dd00052fe6aeeea77f232995d
Richie Tan <richiegenetan14@gmail.com>
2022-02-14 23:29:38 +0800

    Minor Fixes sa Search Page 2

--- pages/search/[id]/index.js
+++ pages/search/[id]/index.js
@@ -291,7 +291,7 @@ export default function SearchResult(){
                                         : null}
                                         </Center>
                                     </Box>
-                                    <Box fontFamily={'Raleway'} p="3" w="100%" bg="light" my='auto'>
+                                    <Box fontFamily={'Raleway'} p="3" w="20%" bg="light" my='auto'>
                                         <Center>
                                         <Text isTruncated>{post.title}</Text>
                                         </Center>
