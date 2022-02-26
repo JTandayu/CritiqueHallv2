8beedf00a429252dffc4cacd567611a20392575d
Richie Tan <richiegenetan14@gmail.com>
2022-02-26 15:40:31 +0800

    Minor Changes in Register 3

--- pages/register.js
+++ pages/register.js
@@ -260,7 +260,7 @@ export default function Register({data2}) {
           // toastIdRef.current = toast({ title: 'Account Creation Unsuccessful!', description: 'Please try again.', status: 'error', duration: 2000, isClosable: true })
             console.log(error.response);
 
-            if(error.response.data.message === "<p>Display Name is already used</p>\n" || error.response.data.message === "<p>Email is already used</p>\n"){
+            if(error.response.data.message == "<p>Display Name is already used</p>\n" || error.response.data.message == "<p>Email is already used</p>\n"){
               // document.getElementById('warning2').removeAttribute('hidden');
               // document.getElementById('warning1').hidden=true;
               // document.getElementById('warning3').hidden=true;
@@ -271,11 +271,11 @@ export default function Register({data2}) {
             //   toastIdRef.current = toast({ position: 'top', title: 'Display Name must not exceed 16 characters! Please enter a valid email address!', status: 'error', duration: 3000, isClosable: true })
             //   return;
             // }
-            if(error.response.data.message === "<p>The Email field must contain a valid email address.</p>\n"){
+            if(error.response.data.message == "<p>The Email field must contain a valid email address.</p>\n"){
               toastIdRef.current = toast({ position: 'top', title: 'Valid email is required.', status: 'error', duration: 3000, isClosable: true })
               return;
             }
-            if(error.response.data.message === "<p>The Display Name field may only contain alpha-numeric characters.</p>\n"){
+            if(error.response.data.message == "<p>The Display Name field may only contain alpha-numeric characters.</p>\n"){
               toastIdRef.current = toast({ position: 'top', title: 'Display Name must contain alphanumeric characters (no special characters).', status: 'error', duration: 3000, isClosable: true })
               return;
             }
